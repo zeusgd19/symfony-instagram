7474d992ff97d84081ce06fa6727658c2b49b832
Alexillo98 <musateduardalexandru@gmail.com>
2024-11-11 08:54:54 +0100

    Cositas profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -18,20 +18,32 @@ class ProfileController extends AbstractController
         return $this->render('page/profile.html.twig',['user' => $user]);
     }
 
-    #[Route('/addFollower/{id}', name: 'add_follower')]
-    public function addFollower(User $userToFollow, ManagerRegistry $doctrine): Response
+    #[Route('/profile/{username}', name: 'profile_page')]
+    public function profile(string $username, ManagerRegistry $doctrine): Response
     {
+        $user = $doctrine->getRepository(User::class)->findOneBy(['username'=>$username]);
+
+        return $this->render('page/profile.html.twig',['user'=>$user]);
+    }
+
+    #[Route('/addFollower/{id}', name: 'add_follower')]
+    public function addFollower(ManagerRegistry $doctrine, int $id): Response
+    {
+        $repository = $doctrine->getRepository(User::class);
+
         $user = $this->getUser();
+        $userToFollow = $repository->find($id);
 
         // Solo seguir si el usuario no se está siguiendo a sí mismo y si aún no lo sigue
-        if ($user !== $userToFollow && !$user->getFollowing()->contains($userToFollow)) {
-            $user->addFollowing($userToFollow);
+        if ($user != $userToFollow) {
+
+            $user->addFollower($userToFollow);
 
             $manager = $doctrine->getManager();
             $manager->persist($user);
             $manager->flush();
         }
 
-        return $this->redirectToRoute('page/profile.html.twig', ['id' => $userToFollow->getId()]);
+        return $this->redirectToRoute('profile');
     }
 }
