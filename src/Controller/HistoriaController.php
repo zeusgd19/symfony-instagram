247d6cd7aed40a3a1eff38f9daf2c7ca7d422c95
zeusgd19 <dariusgd19@gmail.com>
2024-12-04 13:52:25 +0100

    Mensajes directos mas avanzados

--- src/Controller/HistoriaController.php
+++ src/Controller/HistoriaController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Historia;
+use App\Form\StoryFormType;
+use App\Service\FirebaseImageCache;
 use App\Service\FirebaseService;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class HistoriaController extends AbstractController
 {
@@ -18,11 +25,48 @@ class HistoriaController extends AbstractController
     }
 
     #[Route('/historia/nueva', name: 'nueva_historia')]
-    public function index(Request $request): Response
+    public function index(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger,FirebaseImageCache $firebaseImageCache, CacheInterface $cache): Response
     {
-        $usuario = $this->getUser();
-        $archivo = $request->files->get('contenido');
 
+        $story = new Historia();
+        $formulario = $this->createForm(StoryFormType::class,$story);
 
+        $formulario->handleRequest($request);
+
+        if($formulario->isSubmitted() && $formulario->isValid()){
+            $file = $formulario->get('photo')->getData();
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+                $firebasePath = 'stories/' . $newFilename;
+                // Move the file to the directory where images are stored
+                try {
+                    $firebaseUrl = $this->firebaseService->uploadFile($file->getPathname(),$firebasePath);
+
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'file$filename' property to store the PDF file name
+                // instead of its contents
+                $story->setContenido($firebaseUrl);
+            }
+            $story = $formulario->getData();
+            $story->setUser($this->getUser());
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($story);
+            $entityManager->flush();
+
+            return $this->json([
+                'status' => 'success',
+                'message' => 'Post creado con éxito'
+            ]);
+        }
+
+        return $this->render('partials/_storyForm.html.twig', [
+            'form' => $formulario->createView()
+        ]);
     }
 }
