9667b9d4d957ee8c3abe6e2e39f2f3bdcaea5d6f
Alexillo98 <musateduardalexandru@gmail.com>
2024-12-04 09:58:53 +0100

    Story en proceso

---
+++ src/Repository/HistoriaRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Historia;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Historia>
+ *
+ * @method Historia|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Historia|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Historia[]    findAll()
+ * @method Historia[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HistoriaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Historia::class);
+    }
+
+//    /**
+//     * @return Historia[] Returns an array of Historia objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('h')
+//            ->andWhere('h.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('h.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Historia
+//    {
+//        return $this->createQueryBuilder('h')
+//            ->andWhere('h.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
